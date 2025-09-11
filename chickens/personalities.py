54745beedaef07251e2bfaf017a754425ac4afa6
Jayani Srinivasan <srinivasan.jayani@gmail.com>
2025-09-11 09:58:47 -0700

    Create personalities.py

---
+++ chickens/personalities.py
@@ -0,0 +1,61 @@
+# chickens/personalities.py
+"""
+Predefined chicken archetypes for Clucktocracy.
+Each archetype is a dict with:
+    - name (str)
+    - personality (str)
+    - role (str)
+"""
+
+CHICKEN_ARCHETYPES = [
+    {
+        "name": "hen_populist",
+        "personality": "scheming",
+        "role": "populist",
+    },
+    {
+        "name": "hen_technocrat",
+        "personality": "curious",
+        "role": "technocrat",
+    },
+    {
+        "name": "hen_autocrat",
+        "personality": "aggressive",
+        "role": "strongman",
+    },
+    {
+        "name": "hen_diplomat",
+        "personality": "submissive",
+        "role": "mediator",
+    },
+    {
+        "name": "hen_gossip",
+        "personality": "scheming",
+        "role": "gossip",
+    },
+    {
+        "name": "hen_guardian",
+        "personality": "aggressive",
+        "role": "enforcer",
+    },
+    {
+        "name": "hen_zen",
+        "personality": "zen",
+        "role": "pacifist",
+    },
+    {
+        "name": "hen_rebel",
+        "personality": "aggressive",
+        "role": "insurgent",
+    },
+    {
+        "name": "hen_scientist",
+        "personality": "curious",
+        "role": "researcher",
+    },
+    {
+        "name": "hen_trickster",
+        "personality": "scheming",
+        "role": "chaos_agent",
+    },
+]
